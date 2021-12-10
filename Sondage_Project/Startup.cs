ba6543cc684313953056b7346af3c1256c0d8d5f
HILDENBRAND <oliivier68@outlook.fr>
2021-12-10 15:11:47 +0100

    Repair login/logout method

--- Sondage_Project/Startup.cs
+++ Sondage_Project/Startup.cs
@@ -37,10 +37,13 @@ namespace Sondage_Project
 #endif
                 });
 
+            services.AddMvc();
+
             services.AddScoped<ISondageRepository, SondageRepository>();
 
             services.AddControllersWithViews()
                 .AddRazorRuntimeCompilation();
+
             services.AddRazorPages();
         }
 
@@ -57,19 +60,22 @@ namespace Sondage_Project
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
             app.UseAuthentication();
+            app.UseAuthorization();
+            
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }

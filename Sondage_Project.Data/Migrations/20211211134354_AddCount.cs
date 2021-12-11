0eeab6b1fadfd2f1957f5e91a3849ea24fce020f
Hugo Weber <alss-cdaalt21-whu@ccicampus.fr>
2021-12-11 14:44:44 +0100

    commit update table sondage add counter for question

---
+++ Sondage_Project.Data/Migrations/20211211134354_AddCount.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sondage_Project.Data.Migrations
+{
+    public partial class AddCount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "counter_1",
+                table: "Sondages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "counter_2",
+                table: "Sondages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "counter_3",
+                table: "Sondages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "counter_4",
+                table: "Sondages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "counter_1",
+                table: "Sondages");
+
+            migrationBuilder.DropColumn(
+                name: "counter_2",
+                table: "Sondages");
+
+            migrationBuilder.DropColumn(
+                name: "counter_3",
+                table: "Sondages");
+
+            migrationBuilder.DropColumn(
+                name: "counter_4",
+                table: "Sondages");
+        }
+    }
+}
--- Sondage_Project.Data/Models/Sondage.cs
+++ Sondage_Project.Data/Models/Sondage.cs
@@ -13,28 +13,50 @@ namespace Sondage_Project.Data.Models
         [Key]
         public int Id { get; set; }
 
+
         [Required]
         [StringLength(5000)]
         public string Descriptif { get; set; }
+
 
         [Required]
         [StringLength(5000)]
         public string Question_1 { get; set; }
 
+
         [StringLength(5000)]
         public string Question_2 { get; set; }
+
 
         [StringLength(5000)]
         public string Question_3 { get; set; }
 
+
         [StringLength(5000)]
         public string Question_4 { get; set; }
 
+
         [Required]
+        [DefaultValue(0)]
+        public int counter_1 { get; set; }
+
+
+        [DefaultValue(0)]
+        public int counter_2 { get; set; }
+
+
+        [DefaultValue(0)]
+        public int counter_3 { get; set; }
+
+
+        [DefaultValue(0)]
+        public int counter_4 { get; set; }
+
+
         [DefaultValue(true)]
         public bool IsActivated { get; set; }
 
-        [Required]
+
         [DefaultValue(false)]
         public bool MultipleAnswer { get; set; }
 

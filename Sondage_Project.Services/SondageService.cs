2d4a13b475da13f3f8915195ce72b4a12a60eeb5
HILDENBRAND <oliivier68@outlook.fr>
2021-12-11 16:18:56 +0100

    test new branch service

--- Sondage_Project.Services/SondageService.cs
+++ Sondage_Project.Services/SondageService.cs
@@ -32,7 +32,6 @@ namespace Sondage_Project.Services
                 MultipleAnswer = model.MultipleAnswer
             };
 
-            //test commit
             await _repo.AddAsync(sondage);
         }
 

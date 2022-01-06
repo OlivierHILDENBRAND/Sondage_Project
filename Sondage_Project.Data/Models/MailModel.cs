7b539e201e0b95344eb1c8cfbfd76c3888e00f6f
Hugo Weber <alss-cdaalt21-whu@ccicampus.fr>
2022-01-06 11:21:11 +0100

    commit envoie email

---
+++ Sondage_Project.Data/Models/MailModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Sondage_Project.Data.Models
+{
+    public class MailModel
+    {
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Subject { get; set; }
+        
+        public string Body { get; set; }
+        
+    }
+}
---
+++ Sondage_Project/Controllers/EmailController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace Sondage_Project.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: SendMailerController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Index(Sondage_Project.Data.Models.MailModel _objModelMail)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(_objModelMail.To);
+                mail.From = new MailAddress(_objModelMail.From);
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp-relay.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("jurqau@gmail.com", "HEiaiego20*-"); // Enter seders User name and password       
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return View("Index", _objModelMail);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
